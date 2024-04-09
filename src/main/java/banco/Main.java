a1867b229784931a0984e1cb28aff24ed12f1feb
lumo4k <luanmorales321@gmail.com>
2024-04-09 13:23:44 -0300

    Desafio de fixaçcao de section 9 das aulas de Java

---
+++ src/main/java/banco/Main.java
@@ -0,0 +1,47 @@
+package banco;
+
+
+import entities.Conta;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu nome");
+        String nome  = sc.nextLine();
+        System.out.println("Digite seu CPF");
+        String cpf  = sc.nextLine();
+
+        String deposito;
+        double depositoConvertido;
+
+        do {
+            System.out.println("Quanto quer depositar inicialmente?");
+            System.out.println("(Obs: Pode deixar esse valor nulo mas não negativo.)");
+            deposito  = sc.nextLine();
+            System.out.println(deposito);
+        } while (!verificarDouble(deposito));
+
+        depositoConvertido = Double.parseDouble(deposito);
+
+        Conta c1 = new Conta(nome,cpf,depositoConvertido);
+
+        System.out.println(c1.getNome());
+        System.out.println(c1.getCpf());
+        System.out.println(c1.getSaldo());
+
+    }
+
+    public static boolean verificarDouble(String numero) {
+        if (numero == "") {
+            return true;
+        }
+
+        try {
+            Double.parseDouble(numero);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
